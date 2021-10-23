2816e51ef2199c5247a86bb406ce567288e99307
archillexx <ashwinsuresh.x@gmail.com>
2021-10-23 20:14:53 +0530

    Adapt to new theme

--- start_conky.sh
+++ start_conky.sh
@@ -2,8 +2,8 @@
 killall conky
 sleep 5
 
-conky -q -c /home/kryptaculos/.config/conky/conky-1.conf &
-conky -q -c /home/kryptaculos/.config/conky/conky-2.conf
+conky -q -c /home/archillexx/.config/conky/conky-1.conf &
+conky -q -c /home/archillexx/.config/conky/conky-2.conf
 
 
 
